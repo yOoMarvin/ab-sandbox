e337fee06f1f9db1ca34ac9bf275d5e01bfd0394
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2021-03-29 21:44:02 +0200

    Add GA

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,14 @@ import Grid from '../components/Grid'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
+import Head from 'next/head'
+
 export default function Home () {
   return (
     <>
+      <Head>
+        <title>A/B Sandbox</title>
+      </Head>
       <Header />
       <Hero />
       <Feature />
